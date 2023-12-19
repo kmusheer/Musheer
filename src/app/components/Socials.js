c597a01060b0ee0557e116ec5d71c3e750c89e53
kmusheer <99362063+kmusheer@users.noreply.github.com>
2023-12-19 09:46:00 +0530

    Update Socials.js
    just change CV link

--- src/app/components/Socials.js
+++ src/app/components/Socials.js
@@ -3,7 +3,7 @@ import { RiYoutubeLine, RiFacebookLine, RiInstagramLine, RiLinkedinLine, RiTwitt
 
 const Socials = () => {
   return <div className="flex items-center gap-x-5 text-lg ">
-    <Link href={'/Moshir khan CV.pdf'} title="CV Download" className="hover:text-accent transition-all duration-300 animate-pulse hover:text-xl" >
+    <Link href={'/Mohd Moshir.pdf'} title="CV Download" className="hover:text-accent transition-all duration-300 animate-pulse hover:text-xl" >
       <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         <RiDownload2Line />
       </button>
