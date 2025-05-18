91ed22d03e9ed27b421b14a4b27802e24e2c6308
kmusheer <musheerk799@gmail.com>
2025-05-18 15:17:23 +0530

    Add static blogs single data

--- src/app/blogs/[blogid]/page.js
+++ src/app/blogs/[blogid]/page.js
@@ -1,34 +1,79 @@
-import Image from 'next/image';
 import React from 'react';
 import BlogCard from '@/app/components/BlogCard';
-import { getBlog, getBlogs } from '@/utils/fetchBlog';
+import { blogs } from '@/models/Blogs';
 import { notFound } from 'next/navigation';
 
-
 const Page = async ({ params }) => {
-  let singleData = null;
+  const blogId = params.blogid;
 
-  try {
-    singleData = await getBlog(params.blogid);
-  } catch (err) {
-    console.error("Error in getBlog:", err);
-    notFound(); // Shows 404 page if blog not found or fetch fails
-  }
+  // Find the blog from local model
+  const singleData = blogs.find(blog => blog.id === blogId);
+
   if (!singleData) {
-    notFound(); // Handle cases where API returns no blog
+    notFound(); // Trigger 404 if blog not found
   }
-  // console.log('params', params.blogid)
-  const data = await getBlogs();
 
-  const shuffledData = [...data].sort(() => 0.5 - Math.random());
-  // Select a limited number of random blog posts (e.g., 3)
-  const randomBlogs = shuffledData.slice(0, 3);
+  // Get 3 random blogs excluding the current one
+  const randomBlogs = blogs
+    .filter(blog => blog.id !== blogId)
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 3);
 
   return (
-    <>
-      <BlogCard randomBlogs={randomBlogs} singleData={singleData} />
-    </>
+    <BlogCard randomBlogs={randomBlogs} singleData={singleData} />
   );
 };
 
 export default Page;
+
+
+export async function generateStaticParams() {
+  return blogs.map(blog => ({
+    blogid: blog.id,
+  }));
+}
+
+
+
+
+
+
+
+
+
+
+
+// import Image from 'next/image';
+// import React from 'react';
+// import BlogCard from '@/app/components/BlogCard';
+// import { getBlog, getBlogs } from '@/utils/fetchBlog';
+// import { notFound } from 'next/navigation';
+
+
+// const Page = async ({ params }) => {
+//   let singleData = null;
+
+//   try {
+//     singleData = await getBlog(params.blogid);
+//   } catch (err) {
+//     console.error("Error in getBlog:", err);
+//     notFound(); // Shows 404 page if blog not found or fetch fails
+//   }
+//   if (!singleData) {
+//     notFound(); // Handle cases where API returns no blog
+//   }
+//   // console.log('params', params.blogid)
+//   const data = await getBlogs();
+
+//   const shuffledData = [...data].sort(() => 0.5 - Math.random());
+//   // Select a limited number of random blog posts (e.g., 3)
+//   const randomBlogs = shuffledData.slice(0, 3);
+
+//   return (
+//     <>
+//       <BlogCard randomBlogs={randomBlogs} singleData={singleData} />
+//     </>
+//   );
+// };
+
+// export default Page;
